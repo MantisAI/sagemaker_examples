6c23fe7d85802cd2857c7c1abf3a8640b1768d08
Nick Sorros <nsorros@gmail.com>
2022-02-18 12:55:47 +0200

    Fix tests

--- tests/test_train_transformers.py
+++ tests/test_train_transformers.py
@@ -5,4 +5,6 @@ from src.train_transformers import train_transformers
 
 def test_train_pytorch(tmp_path):
     data_path = os.path.join(os.path.dirname(__file__), "data/")
-    train_transformers(data_path, pretrained_model="distilbert-base-uncased", epochs=1)
+    model_path = os.path.join(tmp_path, "pytorch_model.bin")
+    train_transformers(data_path, tmp_path, pretrained_model="distilbert-base-uncased", epochs=1)
+    assert os.path.exists(model_path)
