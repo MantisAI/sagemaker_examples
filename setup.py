6834a4f7c30b7b9cce3570d1d331b610cdc81831
Nick Sorros <nsorros@gmail.com>
2022-02-23 20:07:40 +0200

    Fix sagemaker script by adding src path to root

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="sagemaker_examples", 
-    packages=find_packages()
+    packages=find_packages(where="src"),
+    package_dir={"": "src"}
 )
