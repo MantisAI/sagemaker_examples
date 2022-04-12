1b605ce1db72aa873e4644c72b8a0b5d547d5106
Nick Sorros <nsorros@gmail.com>
2022-04-12 11:56:59 +0300

    Format with black

--- scripts/stop_training.py
+++ scripts/stop_training.py
@@ -6,5 +6,6 @@ def stop_training(job_name):
     session = sagemaker.Session()
     session.stop_training_job(job_name)
 
+
 if __name__ == "__main__":
     typer.run(stop_training)
--- scripts/train_sagemaker.py
+++ scripts/train_sagemaker.py
@@ -5,7 +5,16 @@ from sagemaker.estimator import Estimator
 import typer
 
 
-def train_sagemaker(data_path, model_path, image_uri, entry_point=None, hyperparams=None, instance_type="local", instance_count:int=1, role=os.environ["AWS_SAGEMAKER_ROLE"]):
+def train_sagemaker(
+    data_path,
+    model_path,
+    image_uri,
+    entry_point=None,
+    hyperparams=None,
+    instance_type="local",
+    instance_count: int = 1,
+    role=os.environ["AWS_SAGEMAKER_ROLE"],
+):
     if hyperparams:
         with open(hyperparams) as f:
             hyperparams = json.loads(f.read())
@@ -17,9 +26,10 @@ def train_sagemaker(data_path, model_path, image_uri, entry_point=None, hyperpar
         instance_count=instance_count,
         output_path=model_path,
         hyperparameters=hyperparams,
-        role=role
+        role=role,
     )
     model.fit({"train": data_path})
+
 
 if __name__ == "__main__":
     typer.run(train_sagemaker)
